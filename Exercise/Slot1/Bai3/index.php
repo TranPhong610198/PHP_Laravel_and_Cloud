07823f256075d63b46dd03006290030032b3d894
Moriarty <tphong610198@gmail.com>
2024-03-08 21:35:38 +0700

    commited by Moriarty

--- Exercise/Slot1/Bai3/index.php
+++ Exercise/Slot1/Bai3/index.php
@@ -8,7 +8,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bai 3</title>
 </head>
+<style>
+table, th, td {
+  border:1px solid black;
+  border-collapse: collapse;
+}
+</style>
 <body>
-    
+<table>
+    <?php
+    for ($i = 1; $i < 10; $i++) {
+        echo "<tr>";
+        for ($j = 0; $j < 10; $j++) {
+            if ($i * $j == 0) {
+                echo "<td>&nbsp;&nbsp;&nbsp;&nbsp;</td>";
+            }
+            else echo "<td>". $i * $j ."</td>";
+        }
+        echo "</tr>";
+    }
+    ?>
+</table>
 </body>
 </html>
