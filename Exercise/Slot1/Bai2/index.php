ccd2273310dd8807e2c5ed97289886426588d246
Moriarty <tphong610198@gmail.com>
2024-03-08 16:45:28 +0700

    commited by Moriarty

---
+++ Exercise/Slot1/Bai2/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bai 2</title>
+</head>
+<body>
+    <h2>Enter a Number</h2>
+    <form action="" method="post">
+        <div>
+            <label for="number">Number:</label>
+            <input type="number" name="number"><br>
+            <input type="submit" value="Generate List">
+        </div>
+    </form>
+    <?php
+    if (isset($_POST["number"])) {
+        $number = $_POST["number"];
+
+        echo "<h2>List of Number:</h2>";
+        for ($i = 1; $i <= $number ; $i++){
+            echo "<li>$i</li>";  
+        }
+    }
+    ?>
+</body>
+</html>
